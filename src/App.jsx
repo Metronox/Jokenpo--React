f34a25c89c2dd8a3d90094b8448a2579b22965d2
Metronox <samuksantos1235@gmail.com>
2025-10-31 19:32:34 -0300

    Implementação dos botões e escolhas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const CHOICES = [
+{ id: "rock", label: "Pedra", emoji: "🪨" },
+{ id: "paper", label: "Papel", emoji: "📄" },
+{ id: "scissors", label: "Tesoura", emoji: "✂️" }
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function randomChoiceCPU() {
+  const i = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[i];
 }
 
-export default App
+function theWinner(playerID, cpuID) {
+  if (playerID || cpuID) return null;
+  if (playerID === cpuID) return "draw";
+
+  const winsOver = { rock: "scissors", scissors: "paper", paper: "rock" };
+  return winsOver[playerId] === cpuId ? "win" : "lose";
+}
+
+export default function App() {
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [cpuChoice, setCpuChoice] = useState(null);
+  const outcome = theWinner(playerChoice?.id, cpuChoice?.id);
+
+  const label = outcome === "win" ? "Você ganhou!"
+              : outcome === "lose" ? "Você perdeu."
+              : outcome === "draw" ? "Empate!"
+              : "Faça sua jogada.";
+  
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Jokenpo</h1>
+
+      <div style={{ display: "flex", gap:12, marginTop: 16 }}>
+        {CHOICES.map((c) => (
+          <button key={c.id} onClick={() => setPlayerChoice(c) & setCpuChoice(randomChoiceCPU())}>
+            {c.emoji} {c.label}
+          </button>
+        ))}
+      </div>
+
+      
+      <p style={{ marginTop: 12, fontWeight: 700 }}>{label}</p>
+      <p style={{ marginTop: 12}}>
+        Sua escolha: <strong>{playerChoice?.label ?? "-"}</strong>
+      </p>
+      <p style={{ marginTop: 4}}>
+        Maquina: <strong>{cpuChoice?.label ?? "-"}</strong>
+      </p>
+      
+    </main>
+  )
+}
